dd4c935effce433b23c51765abe8eb6835167123
Mina Nasser <mina.nasser.beshay@gamil.com>
2025-09-21 03:12:08 +0300

    update code

---
+++ src/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#src/test.sh
+
+EXPETED="Hello, Wolrd!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+if [ "$OUTPUT" == "$EXPETED" ]; then
+    echo "✅ Test passed"
+    exit 0
+else
+    echo "❌ Test failed: expected '$EXPETED' but got '$OUTPUT'"
+    exit 1
+fi
